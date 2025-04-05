e23bfedfed19019602eb8d4d1774058a95a4023b
muhammadsalmanfarrisi <muhammadsalman85396@gmail.com>
2025-04-05 17:32:56 +0700

    tambahkan semua data merchant

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 from datetime import datetime
 import datetime
 import pytz
+from collections import defaultdict
 
 
 def format_rupiah(value):
@@ -29,13 +30,13 @@ def ambil_data(url, retries=3):
 
     # Masukkan cookies yang relevan dari browser Anda ke sini
     cookies = {
-        "XSRF-TOKEN": "eyJpdiI6Ikc5U3lJZEhRa3J6TTJiMTBjOUV0RFE9PSIsInZhbHVlIjoicUZXNnJKRk93UU95UXpLYjFWM1J1V3liT2ZOdy8zdXY5Y0lBRGZGWXhxZUduUWhBcEs3TkxzcXZselJtN0NvN2QvMGxVbkNxcFNRK1Q2Y3U3Tm1TUTA2aG9veFk0NU94VnE3d0pDc2I5bnp5dkRyb3VaSUJyWG16QmplakFKRU4iLCJtYWMiOiJkNWIyOGJhZDYyNDU2M2JlNGRhOWE3MGI1OTgxYzk2MDQ3NWU5ZjIwYTBkZjRkMDJlNmQwOTVlN2VhNTZiNjYzIiwidGFnIjoiIn0%3D",
+        "XSRF-TOKEN": "eyJpdiI6InBvd3JYaXBXSko5UTRFbEd1WGVGWGc9PSIsInZhbHVlIjoiUkVCdUJ1WnNxemY5ZlY4bUxOVVVqZDVKdnJIQ25GOERFRlJpalJCN3NYWmNYZGppRXdDUzRUQUF3OGVDbFA1dEtpVFJreXFRdzFqa3ZrWkxhYWNVa0lndDRiT3VCaUg5M204b0plU2JSeDVCUGV4SGw4MVhmYVcwTHBBeThXK2kiLCJtYWMiOiIyNDMyOGY1NGIzMmM3NDRlZWIzNjQwZTkwYzBiMWVmNzU1ZWRkMDlkNDgyMjIzM2RlYTQyNTExYjcwMmQyZDk2IiwidGFnIjoiIn0%3D",
         "_ga": "GA1.1.140722331.1727057868",
         "_ga_JQ088T32QP": "GS1.1.1727061610.2.1.1727061629.0.0.0",
         "_ga_VNWN27RPNX": "GS1.3.1727061611.2.0.1727061611.60.0.0",
-        "ceri_session": "eyJpdiI6ImlBYjE5Q2lYUzZmektETHhUaGIwYXc9PSIsInZhbHVlIjoiUmdpa3RONWxlMGNWNUs3RVV3bUp1RTc5ZVBGK2REa3VwenROYzNhY200YzdzQVRZa1VUK0RpaEhLbFFRMGtzMFM0b3VJMzJzMFRONUhuZWFDakEvVlh0bVFWenFqZExuR0xuUHhkemw2aTFXUE0rbkFxVkNHNEpaQ0F4Ylh6YmciLCJtYWMiOiJmN2Q3NDU3ZDZmNGQ0NmI5ZjFhNmRlZGEzNWNmYjczZTAzZmMwNmM1ODVhMWY2M2IzOGVkOGI2OGJjOGNkOTNlIiwidGFnIjoiIn0%3D",
+        "ceri_session": "eyJpdiI6InBKbGcrRzI2VWdZM2p5WUt1cHVlR2c9PSIsInZhbHVlIjoidyswdTNHMUJhWTFkcDQ1QUFFZ3dKb0Y4RmxvVVNwb3NjbzRtVExCVnhOZmRCMXdSUlZDVlpSMEpvYkI0TWVkckxDOHN6V1lRVGloSmNob0wrajVRWTVGbkV5YjUySU1KaWx3YTk4Q29VRm5kaG9ReWdGSy9ibVg1Ym91WklWTVUiLCJtYWMiOiI4NjNmNjE2MGE5MzIyY2QxYTdmOTIzOTJmMjcxYWU1YTgxZjBmYjBmNzBkYjcwZWQ1Y2ZmYjQ1OTczODU4OTYwIiwidGFnIjoiIn0%3D",
         "cookiesession1": "678B28C4BA1B09254D21278D87A606A5",
-        "jr_cookie": "c686aabd2eea2ae0eedde6ce80f6e467",
+        "jr_cookie": "c686aabd2eea2ae0eedde6ce5c05f167",
         "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d": "eyJpdiI6IlEyVEJBcW9Za2tCUFNJWFpvV2VUNGc9PSIsInZhbHVlIjoiM0NwSGwwdUVwbEVhVGtKVm5hdGJSbUxmSlpUQ0JjZkZPL0pFbEE5K3c3WmtPM3RpZHpkaUlhaldXMThRVlQzNnNKNVcxdTdaTDBMdWttOTlOUDd0cmwvQm50WXBiN2lMVlpSV213Umw2d0lpOUNaSzI4TjNoQ0xraENqRmRON3haWU1ROHVrZTlvZTdxdGM2SUtDME5BPT0iLCJtYWMiOiI0MWUxNTY1MjE5YzBiOTAxZmQwYzcwM2RkMzQwMTViYmU0NDI1OTg5MjY1NDgzZDdmYTliNTMwZWMxNDQwOGUyIiwidGFnIjoiIn0%3D",
     }
 
@@ -79,13 +80,13 @@ def ambil_data_sigap_instansi(url, retries=3):
 
     # Masukkan cookies yang relevan dari browser Anda ke sini
     cookies = {
-        "XSRF-TOKEN": "eyJpdiI6Ikc5U3lJZEhRa3J6TTJiMTBjOUV0RFE9PSIsInZhbHVlIjoicUZXNnJKRk93UU95UXpLYjFWM1J1V3liT2ZOdy8zdXY5Y0lBRGZGWXhxZUduUWhBcEs3TkxzcXZselJtN0NvN2QvMGxVbkNxcFNRK1Q2Y3U3Tm1TUTA2aG9veFk0NU94VnE3d0pDc2I5bnp5dkRyb3VaSUJyWG16QmplakFKRU4iLCJtYWMiOiJkNWIyOGJhZDYyNDU2M2JlNGRhOWE3MGI1OTgxYzk2MDQ3NWU5ZjIwYTBkZjRkMDJlNmQwOTVlN2VhNTZiNjYzIiwidGFnIjoiIn0%3D",
+        "XSRF-TOKEN": "eyJpdiI6InBvd3JYaXBXSko5UTRFbEd1WGVGWGc9PSIsInZhbHVlIjoiUkVCdUJ1WnNxemY5ZlY4bUxOVVVqZDVKdnJIQ25GOERFRlJpalJCN3NYWmNYZGppRXdDUzRUQUF3OGVDbFA1dEtpVFJreXFRdzFqa3ZrWkxhYWNVa0lndDRiT3VCaUg5M204b0plU2JSeDVCUGV4SGw4MVhmYVcwTHBBeThXK2kiLCJtYWMiOiIyNDMyOGY1NGIzMmM3NDRlZWIzNjQwZTkwYzBiMWVmNzU1ZWRkMDlkNDgyMjIzM2RlYTQyNTExYjcwMmQyZDk2IiwidGFnIjoiIn0%3D",
         "_ga": "GA1.1.140722331.1727057868",
         "_ga_JQ088T32QP": "GS1.1.1727061610.2.1.1727061629.0.0.0",
         "_ga_VNWN27RPNX": "GS1.3.1727061611.2.0.1727061611.60.0.0",
-        "ceri_session": "eyJpdiI6ImlBYjE5Q2lYUzZmektETHhUaGIwYXc9PSIsInZhbHVlIjoiUmdpa3RONWxlMGNWNUs3RVV3bUp1RTc5ZVBGK2REa3VwenROYzNhY200YzdzQVRZa1VUK0RpaEhLbFFRMGtzMFM0b3VJMzJzMFRONUhuZWFDakEvVlh0bVFWenFqZExuR0xuUHhkemw2aTFXUE0rbkFxVkNHNEpaQ0F4Ylh6YmciLCJtYWMiOiJmN2Q3NDU3ZDZmNGQ0NmI5ZjFhNmRlZGEzNWNmYjczZTAzZmMwNmM1ODVhMWY2M2IzOGVkOGI2OGJjOGNkOTNlIiwidGFnIjoiIn0%3D",
+        "ceri_session": "eyJpdiI6InBKbGcrRzI2VWdZM2p5WUt1cHVlR2c9PSIsInZhbHVlIjoidyswdTNHMUJhWTFkcDQ1QUFFZ3dKb0Y4RmxvVVNwb3NjbzRtVExCVnhOZmRCMXdSUlZDVlpSMEpvYkI0TWVkckxDOHN6V1lRVGloSmNob0wrajVRWTVGbkV5YjUySU1KaWx3YTk4Q29VRm5kaG9ReWdGSy9ibVg1Ym91WklWTVUiLCJtYWMiOiI4NjNmNjE2MGE5MzIyY2QxYTdmOTIzOTJmMjcxYWU1YTgxZjBmYjBmNzBkYjcwZWQ1Y2ZmYjQ1OTczODU4OTYwIiwidGFnIjoiIn0%3D",
         "cookiesession1": "678B28C4BA1B09254D21278D87A606A5",
-        "jr_cookie": "c686aabd2eea2ae0eedde6ce80f6e467",
+        "jr_cookie": "c686aabd2eea2ae0eedde6ce5c05f167",
         "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d": "eyJpdiI6IlEyVEJBcW9Za2tCUFNJWFpvV2VUNGc9PSIsInZhbHVlIjoiM0NwSGwwdUVwbEVhVGtKVm5hdGJSbUxmSlpUQ0JjZkZPL0pFbEE5K3c3WmtPM3RpZHpkaUlhaldXMThRVlQzNnNKNVcxdTdaTDBMdWttOTlOUDd0cmwvQm50WXBiN2lMVlpSV213Umw2d0lpOUNaSzI4TjNoQ0xraENqRmRON3haWU1ROHVrZTlvZTdxdGM2SUtDME5BPT0iLCJtYWMiOiI0MWUxNTY1MjE5YzBiOTAxZmQwYzcwM2RkMzQwMTViYmU0NDI1OTg5MjY1NDgzZDdmYTliNTMwZWMxNDQwOGUyIiwidGFnIjoiIn0%3D",
     }
 
@@ -129,6 +130,206 @@ def ambil_data_sigap_instansi(url, retries=3):
         return None
 
 
+def ambil_data_merchant(url, retries=3):
+    session = requests.Session()
+
+    # Masukkan cookies yang relevan dari browser Anda ke sini
+    cookies = {
+        "XSRF-TOKEN": "eyJpdiI6IkJlZkFYMi9YWTdBcmVBSUYram5nTWc9PSIsInZhbHVlIjoiUXpFZGNXMEJ3QXFRcUhkVXJKSW5YK3hURHBCYldoQXova2FNakxsc3NxbmRMUWFyMWxPbTJKYW5lOUtkM28rTVZTUmlzYjZjZ2YwUi9TQXlLUWNHa3d0bUlpNkxGNTM5My85dmJXTEFFMlNzdmFlUHUzaWlzNmcyYXd2alM2NnQiLCJtYWMiOiJlNTA1ZWFmMWU3Y2UzOTQ2ZWVmZDc3ODFjYjk2YzE5NjFmMWRhODI3Zjc5YjAxZmIyMzU2ODMxNzU3NWYxYzUxIiwidGFnIjoiIn0%3D",
+        "_ga": "GA1.1.140722331.1727057868",
+        "_ga_JQ088T32QP": "GS1.1.1727061610.2.1.1727061629.0.0.0",
+        "_ga_VNWN27RPNX": "GS1.3.1727061611.2.0.1727061611.60.0.0",
+        "cookiesession1": "678B28E3D57096F3CACCD7149E1DBECE",
+        "jrreward_session": "eyJpdiI6ImZEV3NCR0RjNnVDN2F0VjlmdmhHT0E9PSIsInZhbHVlIjoibWJReVpzbUhSRG1WN3N1VDN6emF3cHVsTUhiSUsrckxDaFpGa0duSFpKZFBlWlJKczh5VXBwWmxzQS95eGY1OXA4WXNUZG5WRmlmWWJxbS9nSTFWVzh2R09iYjVBUnA0dDZRUTdDSUhJcFJNTXZZMlZFV2xlc2RYTVpLTEZUSngiLCJtYWMiOiIyNTdjZjAyZmZiODYyYWUyMTE3MDZjNjQ1ZDUxMzYwMjcwZTQ4NzAyMjg0MDUxZmVmMjBmMmFhMDlmNThkZWIxIiwidGFnIjoiIn0%3D",
+        "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d": "eyJpdiI6IlVGZ3ViU3VKbWR4YW1adWpucjkzNVE9PSIsInZhbHVlIjoialF5blJ6blBxdG9DVTBuRG05ZGROWmFzUWNNVEpjUXpINk1TKzNhZ0ZnUVA1d2FvdzdwQnAzeWtoeUpCNWZrT1phc1JvTWNOZ2szRVpwWUtmcGY0RCtJaC92a1VrclhtMmRWbTNhSnQ3NXczcGdpUFo0SXVtY1hqWmNUa1dPdysxUWpzTjNrNVBFRjlzbTNWeW1mSTRVYkpDTXpHRjNTd1p0VG1IYldnbkc1bnpITVcwNkI4dC8rYVhlTmRiK0p1MFpMT1pKbTMvQmZlUUN5MnV2NnM5Sk10WUlOQm15SUNHNlJnV1ZhbjRkUT0iLCJtYWMiOiI4ZjdiZGZjNzc3MDNhY2I1ZjAzZGM1Y2E5YTA2MjM2OTM5NGFjOGFiZDBiNzg3OWVjMGJkMmExZTliNzY3OTU2IiwidGFnIjoiIn0%3D",
+    }
+
+    session.cookies.update(cookies)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+    }
+
+    try:
+        response = session.get(url, headers=headers)
+
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                result = []
+
+                for item in data["data"]:
+                    result.append(
+                        {
+                            "merchant_name": item["merchant_name"],
+                            "campaign_name": item["campaign_name"],
+                            "name": item["name"],
+                            "no_polisi": item["no_polisi"],
+                            "email": item["email"],
+                            "code": item["code"],
+                            "status": item["status"],
+                            "created_at": item["created_at"],
+                            "area": item["area"],
+                        }
+                    )
+
+                return result
+            except ValueError:
+                print("Response tidak dalam format JSON")
+                return None
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
+        return None
+
+
+def ambil_data_merchant_merchant(url, retries=3):
+    session = requests.Session()
+
+    # Masukkan cookies yang relevan dari browser Anda ke sini
+    cookies = {
+        "XSRF-TOKEN": "eyJpdiI6IkJlZkFYMi9YWTdBcmVBSUYram5nTWc9PSIsInZhbHVlIjoiUXpFZGNXMEJ3QXFRcUhkVXJKSW5YK3hURHBCYldoQXova2FNakxsc3NxbmRMUWFyMWxPbTJKYW5lOUtkM28rTVZTUmlzYjZjZ2YwUi9TQXlLUWNHa3d0bUlpNkxGNTM5My85dmJXTEFFMlNzdmFlUHUzaWlzNmcyYXd2alM2NnQiLCJtYWMiOiJlNTA1ZWFmMWU3Y2UzOTQ2ZWVmZDc3ODFjYjk2YzE5NjFmMWRhODI3Zjc5YjAxZmIyMzU2ODMxNzU3NWYxYzUxIiwidGFnIjoiIn0%3D",
+        "_ga": "GA1.1.140722331.1727057868",
+        "_ga_JQ088T32QP": "GS1.1.1727061610.2.1.1727061629.0.0.0",
+        "_ga_VNWN27RPNX": "GS1.3.1727061611.2.0.1727061611.60.0.0",
+        "cookiesession1": "678B28E3D57096F3CACCD7149E1DBECE",
+        "jrreward_session": "eyJpdiI6ImZEV3NCR0RjNnVDN2F0VjlmdmhHT0E9PSIsInZhbHVlIjoibWJReVpzbUhSRG1WN3N1VDN6emF3cHVsTUhiSUsrckxDaFpGa0duSFpKZFBlWlJKczh5VXBwWmxzQS95eGY1OXA4WXNUZG5WRmlmWWJxbS9nSTFWVzh2R09iYjVBUnA0dDZRUTdDSUhJcFJNTXZZMlZFV2xlc2RYTVpLTEZUSngiLCJtYWMiOiIyNTdjZjAyZmZiODYyYWUyMTE3MDZjNjQ1ZDUxMzYwMjcwZTQ4NzAyMjg0MDUxZmVmMjBmMmFhMDlmNThkZWIxIiwidGFnIjoiIn0%3D",
+        "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d": "eyJpdiI6IlVGZ3ViU3VKbWR4YW1adWpucjkzNVE9PSIsInZhbHVlIjoialF5blJ6blBxdG9DVTBuRG05ZGROWmFzUWNNVEpjUXpINk1TKzNhZ0ZnUVA1d2FvdzdwQnAzeWtoeUpCNWZrT1phc1JvTWNOZ2szRVpwWUtmcGY0RCtJaC92a1VrclhtMmRWbTNhSnQ3NXczcGdpUFo0SXVtY1hqWmNUa1dPdysxUWpzTjNrNVBFRjlzbTNWeW1mSTRVYkpDTXpHRjNTd1p0VG1IYldnbkc1bnpITVcwNkI4dC8rYVhlTmRiK0p1MFpMT1pKbTMvQmZlUUN5MnV2NnM5Sk10WUlOQm15SUNHNlJnV1ZhbjRkUT0iLCJtYWMiOiI4ZjdiZGZjNzc3MDNhY2I1ZjAzZGM1Y2E5YTA2MjM2OTM5NGFjOGFiZDBiNzg3OWVjMGJkMmExZTliNzY3OTU2IiwidGFnIjoiIn0%3D",
+    }
+
+    session.cookies.update(cookies)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+    }
+
+    try:
+        response = session.get(url, headers=headers)
+
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                result = []
+
+                for item in data["data"]:
+                    result.append(
+                        {
+                            "data_id": item["data_id"],
+                            "users": item["users"],
+                            "phone": item["phone"],
+                            "cities_id": item["cities_id"],
+                            "users_id": item["users_id"],
+                            "name": item["name"],
+                            "wilayah": item["wilayah"],
+                            "category_name": item["category_name"],
+                            "status": item["status"],
+                        }
+                    )
+
+                return result
+            except ValueError:
+                print("Response tidak dalam format JSON")
+                return None
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
+        return None
+
+
+WILAYAH_KE_KANTOR = {
+    # 0400 - KANTOR WILAYAH JAWA TENGAH
+    "Demak": "0400 - KANTOR WILAYAH JAWA TENGAH",
+    "Grobogan": "0400 - KANTOR WILAYAH JAWA TENGAH",
+    "Kendal": "0400 - KANTOR WILAYAH JAWA TENGAH",
+    "Salatiga": "0400 - KANTOR WILAYAH JAWA TENGAH",
+    # 0401 - KANTOR CABANG SURAKARTA
+    "Boyolali": "0401 - KANTOR CABANG SURAKARTA",
+    "Klaten": "0401 - KANTOR CABANG SURAKARTA",
+    "Sragen": "0401 - KANTOR CABANG SURAKARTA",
+    "Surakarta": "0401 - KANTOR CABANG SURAKARTA",
+    # 0402 - KANTOR CABANG MAGELANG
+    "Kebumen": "0402 - KANTOR CABANG MAGELANG",
+    "Magelang": "0402 - KANTOR CABANG MAGELANG",
+    "Purworejo": "0402 - KANTOR CABANG MAGELANG",
+    "Temanggung": "0402 - KANTOR CABANG MAGELANG",
+    "Wonosobo": "0402 - KANTOR CABANG MAGELANG",
+    # 0403 - KANTOR CABANG PURWOKERTO
+    "Banjarnegara": "0403 - KANTOR CABANG PURWOKERTO",
+    "Banyumas": "0403 - KANTOR CABANG PURWOKERTO",
+    "Cilacap": "0403 - KANTOR CABANG PURWOKERTO",
+    "Purbalingga": "0403 - KANTOR CABANG PURWOKERTO",
+    # 0404 - KANTOR CABANG PEKALONGAN
+    "Batang": "0404 - KANTOR CABANG PEKALONGAN",
+    "Brebes": "0404 - KANTOR CABANG PEKALONGAN",
+    "Pekalongan": "0404 - KANTOR CABANG PEKALONGAN",
+    "Pemalang": "0404 - KANTOR CABANG PEKALONGAN",
+    "Tegal": "0404 - KANTOR CABANG PEKALONGAN",
+    # 0405 - KANTOR CABANG PATI
+    "Blora": "0405 - KANTOR CABANG PATI",
+    "Jepara": "0405 - KANTOR CABANG PATI",
+    "Kudus": "0405 - KANTOR CABANG PATI",
+    "Pati": "0405 - KANTOR CABANG PATI",
+    "Rembang": "0405 - KANTOR CABANG PATI",
+    # 0406 - KANTOR CABANG SEMARANG
+    "Semarang": "0406 - KANTOR CABANG SEMARANG",
+    # 0407 - KANTOR CABANG SUKOHARJO
+    "Karanganyar": "0407 - KANTOR CABANG SUKOHARJO",
+    "Sukoharjo": "0407 - KANTOR CABANG SUKOHARJO",
+    "Wonogiri": "0407 - KANTOR CABANG SUKOHARJO",
+}
+
+
+def hitung_jumlah_merchant_per_kantor_detail(data_merchant):
+    hasil = {kode: {"jumlah": 0, "data": []} for kode in WILAYAH_KE_KANTOR.values()}
+
+    for merchant in data_merchant:
+        wilayah = merchant.get("wilayah", "")
+        kata_pertama = wilayah.split(",")[0].strip()
+        kantor = WILAYAH_KE_KANTOR.get(kata_pertama, "LAINNYA / TIDAK DIKETAHUI")
+
+        hasil.setdefault(kantor, {"jumlah": 0, "data": []})
+        hasil[kantor]["jumlah"] += 1
+        hasil[kantor]["data"].append(merchant)
+
+    return hasil
+
+
+def hitung_jumlah_merchant_diklaim(data_merchant):
+    hasil = {kode: {"jumlah": 0, "data": []} for kode in WILAYAH_KE_KANTOR.values()}
+
+    for merchant in data_merchant:
+        wilayah = merchant.get("area", "")
+        kata_pertama = wilayah.split(",")[0].strip()
+        kantor = WILAYAH_KE_KANTOR.get(kata_pertama, "LAINNYA / TIDAK DIKETAHUI")
+
+        hasil.setdefault(kantor, {"jumlah": 0, "data": []})
+        hasil[kantor]["jumlah"] += 1
+        hasil[kantor]["data"].append(merchant)
+
+    return hasil
+
+
+def hitung_jumlah_merchant_diklaim_selesai(data_merchant):
+    hasil = {kode: {"jumlah": 0, "data": []} for kode in WILAYAH_KE_KANTOR.values()}
+
+    for merchant in data_merchant:
+        # Tambahkan kondisi untuk memeriksa status adalah "approve"
+        if merchant.get("status") == "approve":
+            wilayah = merchant.get("area", "")
+            kata_pertama = wilayah.split(",")[0].strip()
+            kantor = WILAYAH_KE_KANTOR.get(kata_pertama, "LAINNYA / TIDAK DIKETAHUI")
+
+            hasil.setdefault(kantor, {"jumlah": 0, "data": []})
+            hasil[kantor]["jumlah"] += 1
+            hasil[kantor]["data"].append(merchant)
+
+    return hasil
+
+
 @app.context_processor
 def utility_processor():
     return dict(format_rupiah=format_rupiah)
@@ -158,6 +359,28 @@ def index():
 
     data_2024_sigap_instansi = None
 
+    data_2024_merchant = None
+    data_2024_merchant_merchant = None
+    data_per_kantor = None
+    data_per_kantor_diklaim = None
+    data_per_kantor_diklaim_selesai = None
+    url_2024_merchant_merchant = f"https://merchant.jasaraharja.co.id/merchant/datatables?draw=1&columns%5B0%5D%5Bdata%5D=name&columns%5B0%5D%5Bname%5D=name&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=category_name&columns%5B1%5D%5Bname%5D=category_name&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=phone&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=wilayah&columns%5B3%5D%5Bname%5D=wilayah&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=picture&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=status&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=6&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=1000&search%5Bvalue%5D=&search%5Bregex%5D=false&category_id=all&province_id=all&_=174263731193"
+    data_2024_merchant_merchant = ambil_data_merchant_merchant(
+        url_2024_merchant_merchant
+    )
+    data_per_kantor = hitung_jumlah_merchant_per_kantor_detail(
+        data_2024_merchant_merchant
+    )
+
+    url_2024_merchant = f"https://merchant.jasaraharja.co.id/home/datatables?"
+
+    data_2024_merchant = ambil_data_merchant(url_2024_merchant)
+
+    data_per_kantor_diklaim = hitung_jumlah_merchant_diklaim(data_2024_merchant)
+    data_per_kantor_diklaim_selesai = hitung_jumlah_merchant_diklaim_selesai(
+        data_2024_merchant
+    )
+
     if request.method == "POST":
 
         start_date_2024 = request.form.get("start_date_2024", start_date_2024)
@@ -181,6 +404,9 @@ def index():
         today_form=today_form,
         data_2024_sigap_instansi=data_2024_sigap_instansi or [],
         data_2024=data_2024 or [],
+        data_kantor=data_per_kantor,
+        data_per_kantor_diklaim=data_per_kantor_diklaim,
+        data_per_kantor_diklaim_selesai=data_per_kantor_diklaim_selesai,
     )
 
 
